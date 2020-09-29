387b6bc7b43b6e091368fb090e2f76126c7794db
kelvintechie1 <41085962+kelvintechie1@users.noreply.github.com>
2020-09-29 09:08:02 -0500

    Adding Bash Folder & YYC Challenge

---
+++ Bash/yyc-bashchallenge.sh
@@ -0,0 +1,37 @@
+kelvintr@kokochap-server:~/yyc$ cat checksum.sh
+#!/bin/bash
+
+# Provide the directory you want to use as an argument to this command - otherwise, the current folder will be used.
+
+DIRECTORY=""
+
+if [ $# -eq 0 ]
+then
+        DIRECTORY=$(pwd)
+else
+        DIRECTORY=$1
+fi
+
+FILES=$(ls -1 ${DIRECTORY})
+
+for FILE in ${FILES}
+do
+        i=${DIRECTORY}/${FILE}
+
+        CS=$(md5sum ${i} | awk '{ print $1 }')
+
+        OUTPUT+=${CS}": "
+
+        STAT=`stat --format=%a ${i} | grep -E "7$"`
+
+        if ((${STAT} -ne ""))
+        then
+                OUTPUT+="WARNING: FILE IS WORLD WRITABLE: "
+        fi
+
+        OUTPUT+=${i}
+ 
+        echo ${OUTPUT}
+
+        OUTPUT=""
+done
