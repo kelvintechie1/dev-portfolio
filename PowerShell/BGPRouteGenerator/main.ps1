86b6f7bb2d9aa083b8fe452deac8dbb7eacb3aa9
Kelvin Tran <kelvin.tran1234@gmail.com>
2021-05-24 14:26:25 -0500

    Add name to Routing Policy

--- PowerShell/BGPRouteGenerator/main.ps1
+++ PowerShell/BGPRouteGenerator/main.ps1
@@ -56,7 +56,7 @@ foreach ($route in $bgpRoutes) {
     if ((($minimumPrepend -ne 0) -and ($maximumPrepend -ne 0)) -and ($minimumPrepend -le $maximumPrepend)) {
         $prependAmount = Get-Random -Minimum $minimumPrepend -Maximum $maximumPrepend
         if ($prependAmount -ne 0) {
-            Add-BgpRoutingPolicy -MatchPrefix $route -PolicyType ModifyAttribute -AddCommunity ("65001:10" + $prependAmount.ToString())
+            Add-BgpRoutingPolicy -Name $route -MatchPrefix $route -PolicyType ModifyAttribute -AddCommunity ("65001:10" + $prependAmount.ToString())
         }
     }
 }
